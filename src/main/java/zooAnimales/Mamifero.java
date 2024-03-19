2487f806529592787d9af4b6ca408ba59a2b6377
Valeryfer <159395606+Valeryfer@users.noreply.github.com>
2024-03-19 00:45:36 -0500

    Intento 9

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -43,7 +43,7 @@ public class Mamifero extends Animal {
 		return listado;
 	}
 
-    public void isPelaje(boolean pelaje) {
+    public void setPelaje(boolean pelaje) {
         this.pelaje = pelaje;
     }
 
@@ -51,7 +51,7 @@ public class Mamifero extends Animal {
         this.patas = patas;
     }
 
-    public boolean getPelaje() {
+    public boolean isPelaje() {
         return pelaje;
     }
 
